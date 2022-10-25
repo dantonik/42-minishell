58a2b4a1250dae909db1a6a582253cf79984d80b
Dennis Antonik <dantonik@1-B-12.42heilbronn.de>
2022-10-25 02:11:23 +0200

    reworked

---
+++ MS_NEW/src/main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dantonik <dantonik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 22:12:26 by dantonik          #+#    #+#             */
+/*   Updated: 2022/10/25 00:44:39 by dantonik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*input;
+	t_head	*head;
+	// t_env_head *env_head;
+
+	if (argc != 1)
+	{
+		printf("trash: %s: No such file or directory\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	if (envp[0] == NULL)
+	{
+		printf("trash: send the environment please!\n");
+		return (EXIT_FAILURE);
+	}
+	head = (t_head *)malloc(sizeof(t_head));
+	(void) argv;
+	while (1)
+	{
+		input = NULL;
+		input = readline(MINISHELL);
+		create_list(&head, input);
+		printl(head);
+		free_list_loop(&head);
+	}
+	free(head);
+	return (0);
+}
